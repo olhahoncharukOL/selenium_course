dbc827c25c1c5794c923e2ec7040e61c7726d8de
Olga <goncharukkiev@gmail.com>
2018-04-24 10:15:00 +0300

    task7++

--- src/test/java/com/olga/MenuTest.java
+++ src/test/java/com/olga/MenuTest.java
@@ -6,12 +6,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by User on 18.04.2018.
  */
-public class MenuTest extends SetDriver{
+public class MenuTest extends SetDriver {
     List<WebElement> menuListOfItems;
     WebElement menuItem;
     List<WebElement> menuItemSubitems;
@@ -22,44 +23,62 @@ public class MenuTest extends SetDriver{
         LogginTest test = new LogginTest();
         test.loggin();
         findMenuListOfItems();
-      for (int i=0; i< menuListOfItems.size(); i++) {
-          menuItem= menuListOfItems.get(i);
-          menuItem.click();
+        for (int i = 0; i < menuListOfItems.size(); i++) {
+            findMenuListOfItems();
+            menuItem = menuListOfItems.get(i);
+            menuItem.click();
 
-          AssertionError error = new AssertionError();
-          if(driver.getTitle().equals("")) {
-              System.out.println("There is no title on the page " + driver.getCurrentUrl()+". "+error.getMessage());
-              Assert.fail();
-          }
-          System.out.println("Title of the page "+ driver.getCurrentUrl()+ "is: "+driver.getTitle());
+            AssertionError error = new AssertionError();
+            if (isElementPresent(By.tagName("h1"))==false) {
+                System.out.println("There is no title (h1) on the page " + driver.getCurrentUrl() + ". " + error.getMessage());
+                Assert.fail();
+            }
+            System.out.println("Title (h1) of the page " + driver.getCurrentUrl() + " is: "
+                    + driver.findElement(By.tagName("h1")).getText());
 
-          findMenuListOfItems();
+            findMenuListOfItems();
 
-          if ((driver.findElements(By.cssSelector("ul.docs li")).size() > 0)) {
-              findMenuItemSubitems();
-              for(int k=1; k<menuItemSubitems.size(); k++) {
-                  menuSubItem=menuItemSubitems.get(k);
-                  menuSubItem.click();
-                  if(driver.getTitle().equals("")) {
-                      System.out.println("There is no title on the page " + driver.getCurrentUrl()+". "+error.getMessage());
-                      Assert.fail();
-                  }
+            if ((driver.findElements(By.cssSelector("ul.docs li")).size() > 0)) {
+                findMenuItemSubitems();
+                for (int k = 1; k < menuItemSubitems.size(); k++) {
+                    findMenuItemSubitems();
+                    menuSubItem = menuItemSubitems.get(k);
+                    menuSubItem.click();
 
-                  System.out.println("Title of the page "+ driver.getCurrentUrl()+ "is: "+driver.getTitle());
+                    if (isElementPresent(By.tagName("h1"))==false) {
+                        System.out.println("There is no title on the page " + driver.getCurrentUrl() + ". "
+                                + error.getMessage());
+                        Assert.fail();
+                    }
 
-                  findMenuItemSubitems();
-              }
-          }
-          findMenuListOfItems();
-      }
+                    System.out.println("Title (h1) of the page " + driver.getCurrentUrl() + " is: "
+                            + driver.findElement(By.tagName("h1")).getText());
+                }
+            }
+        }
     }
+
     public List<WebElement> findMenuListOfItems() {
+        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
         menuListOfItems = driver.findElements(By.cssSelector("#app-"));
         return menuListOfItems;
     }
 
     public List<WebElement> findMenuItemSubitems() {
+        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
         menuItemSubitems = driver.findElements(By.cssSelector("#box-apps-menu li li"));
         return menuItemSubitems;
     }
+
+    public boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException e) {
+            return  false;
+        }
     }
+}
+
+
+
